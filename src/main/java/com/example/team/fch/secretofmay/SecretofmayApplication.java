62000f9e11d78b113a606bde6bfa7054dd2f147e
hej090224 <fc49854985@gmail.com>
2026-05-06 14:51:47 +0900

    refactor: 실행 클래스명 변경

--- src/main/java/com/example/team/fch/secretofmay/SecretofmayApplication.java
+++ src/main/java/com/example/team/fch/secretofmay/SecretofmayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SomApplication {
+public class SecretofmayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SomApplication.class, args);
+        SpringApplication.run(SecretofmayApplication.class, args);
     }
 
 }
