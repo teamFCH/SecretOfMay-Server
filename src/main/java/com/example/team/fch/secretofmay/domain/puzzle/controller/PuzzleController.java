a838788ad1d40a5b2e03eb9c9391147bff4df147
hej090224 <fc49854985@gmail.com>
2026-05-08 14:08:44 +0900

    refactor: 불필요한 임포트 제거

--- src/main/java/com/example/team/fch/secretofmay/domain/puzzle/controller/PuzzleController.java
+++ src/main/java/com/example/team/fch/secretofmay/domain/puzzle/controller/PuzzleController.java
@@ -8,7 +8,6 @@ import com.example.team.fch.secretofmay.domain.puzzle.service.SubmitPuzzleServic
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/com/example/team/fch/secretofmay/domain/puzzle/service/impl/SubmitPuzzleServiceImpl.java
+++ src/main/java/com/example/team/fch/secretofmay/domain/puzzle/service/impl/SubmitPuzzleServiceImpl.java
@@ -15,7 +15,6 @@ import com.example.team.fch.secretofmay.global.exception.GlobalException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
